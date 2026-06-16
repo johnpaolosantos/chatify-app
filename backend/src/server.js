b2a93edc499f10bc8ab8eac2851216844c64fc82
johnpaolosantos <johnpaolosantos527@gmail.com>
2026-06-16 15:29:03 +0800

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 import path from 'path';
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
@@ -22,4 +24,7 @@ if (process.env.NODE_ENV === "production") {
     })
 }
 
-app.listen(PORT, () => console.log("Server running on port: " + PORT));
+app.listen(PORT, () => {
+    console.log("Server running on port: " + PORT);
+    connectDB();
+});
