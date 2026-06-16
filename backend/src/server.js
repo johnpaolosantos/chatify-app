a06f38649b30448f67e8df37c71dd0a92e44b51b
johnpaolosantos <johnpaolosantos527@gmail.com>
2026-06-16 14:10:37 +0800

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,4 +21,5 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
     })
 }
+
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
